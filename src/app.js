10116da2d626b7afe4a0bf5b77a2ba460c1bd21b
Saha <suvankar.saha@sap.com>
2021-08-17 17:37:39 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const geoCode = require('./utils/geocode')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewDirectortyPath = path.join(__dirname, '../templates/views')
